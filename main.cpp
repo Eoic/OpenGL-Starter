c355604cdb473b54fb6397f0a05349101b7ee003
Eoic <karolis.strazdas@protonmail.com>
2024-05-22 20:23:24 +0300

    Make window floating by default (for i3).

--- main.cpp
+++ main.cpp
@@ -20,6 +20,7 @@ int main() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
     GLFWwindow* window = glfwCreateWindow(800, 600, "Hello GLEW and GLFW", NULL, NULL);
 
@@ -39,12 +40,11 @@ int main() {
 
     cout << "OpenGL version: " << glGetString(GL_VERSION) << endl;
 
-    glClearColor(3.0f, 0.8f, 0.5f, 1.0f);
-
     while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+        glClearColor(3.0f, 0.8f, 0.5f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         glfwSwapBuffers(window);
-        glfwPollEvents();
     }
 
     glfwDestroyWindow(window);
